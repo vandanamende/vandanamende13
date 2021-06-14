afbdaa25f8e09818a402caa910517531eb85b3c6
vandanamende <85801315+vandanamende@users.noreply.github.com>
2021-06-14 09:24:44 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,30 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("create linkedin")
+create linkedin
+>>> print("create github")
+create github
+>>> name="vandana mende"
+>>> name
+'vandana mende'
+>>> print("name")
+name
+>>> 2+3
+5
+>>> 2*5
+10
+>>> 2**5
+32
+>>> 2\1
+SyntaxError: unexpected character after line continuation character
+>>> 2/1
+2.0
+>>> name='vandana'
+>>> name2='yashu'
+>>> name+name2
+'vandanayashu'
+>>> 6%2
+0
+>>> print("completed basic program")
+completed basic program
+>>> 
