b83b95015dec3e89087fbbbd7bcfcb6778fd389d
vandanamende <85801315+vandanamende@users.noreply.github.com>
2021-06-16 18:08:16 +0530

    Add files via upload

---
+++ day4assign(1).py
@@ -0,0 +1,25 @@
+#1
+a=b=c=30
+print("value of a=" ,a,"b=",b,"c=",c, end='\n')
+print(a/10)
+print(b*50)
+print(c+60)
+
+#2
+string="variable"
+print(string)
+print("replacing e in string = ",string.replace("e","s"))
+
+#3
+x=60
+y=5.9
+x=int(y)
+print(x)
+y=float(x)
+print(y)
+#3
+a=40
+b=50.3
+print("convert int into float a: " ,float(a))
+print("convert float into int b: " ,int(b))
+
